5bc18931503caf3cf2712e0e0a32eda764e4736b
Vincent DUSHIME <vincentdx18@gmail.com>
2024-07-10 08:14:40 +0200

    update

--- javascriptaio/chapter_1/gamelet.js
+++ javascriptaio/chapter_1/gamelet.js
@@ -1,7 +1,22 @@
+/*
+Gamelet: a starting point for writing games
+Author: Chris Minnick
+Version: 1.0
+Instructions:
+Include gamelet.js in an HTML document containing
+an element with an id of 'ball'.
+The script will detect when the left or right arrow
+key is pressed and will move the ball element
+accordingly.
+*/
+
 const ball = document.getElementById("ball");
 document.addEventListener("keydown", handleKeyPress);
 let position = 0;
-
+/*
+handleKeyPress
+responds to certain key presses by updating position.
+*/
 function handleKeyPress(e) {
   if (e.code === "ArrowLeft") {
     position = position - 10;
@@ -14,6 +29,11 @@ function handleKeyPress(e) {
   }
   refresh();
 }
+
+/*
+refresh
+changes the position of the ball
+*/
 function refresh() {
   ball.style.left = position + "px";
 }
