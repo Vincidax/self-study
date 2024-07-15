8c880730922b5ba7ecd9648152404f2819174676
Vincent DUSHIME <vincentdx18@gmail.com>
2024-07-15 08:52:14 +0200

    exercise

---
+++ javascriptaio/chapter_8/listing_8_1.js
@@ -0,0 +1,9 @@
+const favoriteFood = "tacos";
+
+makeDinner(favoriteFood);
+
+function makeDinner(whatToMake) {
+  console.log(`I see you want to make ${whatToMake}.`);
+  whaToMake = "salad";
+  console.log(`I've decided to make ${whatToMake} instead.`);
+}
