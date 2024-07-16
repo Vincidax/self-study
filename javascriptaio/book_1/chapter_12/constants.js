661a58dbf507ceda8fe134fabe6d90cd253864d8
Vincidax <vincentdx18@gmail.com>
2024-07-16 15:19:14 +0200

    book_2

---
+++ javascriptaio/book_1/chapter_12/constants.js
@@ -0,0 +1,22 @@
+export const API = "https://api.example.com";
+
+export const colorThemeDrk = {
+  backgroundColor: "#000",
+  textColor: "#000",
+  headerColor: "#FF0000",
+  footerColor: "#FF0000",
+};
+
+export const colorThemeLght = {
+  backgroundColor: "#fff",
+  textColor: "#333",
+  headerColor: "#FF0000",
+  footerColor: "#FF0000",
+};
+
+export const labels = {
+  english: {
+    supportLink: "Get Support",
+    ContactLink: "Contact Us",
+  },
+};
---
+++ javascriptaio/book_1/chapter_12/listing_12_3.js
@@ -0,0 +1,7 @@
+export const stateName = "Oregon";
+export const capitalCity = "Salem";
+export const stateBird = "Western meadowlark";
+
+export default function getStateInfo() {
+  return { stateName, capitalCity, stateBird };
+}
