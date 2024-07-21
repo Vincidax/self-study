d3143f60d762c236d50c8faaeb5fb3448ed587c4
Vincent DUSHIME <vincentdx18@gmail.com>
2024-07-21 12:57:57 +0200

    novice

--- javascriptaio/book_3/chapter_2/my-react-app/src/App.jsx
+++ javascriptaio/book_3/chapter_2/my-react-app/src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import SearchForm from "./SearchForm";
-import Message from "./Message";
+import WelcomeScreen from "./WelcomeScreen";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <SearchForm />
-      <Message />
+      <WelcomeScreen />
     </>
   );
 }
---
+++ novice_to_ninja/chap_3/hello.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+// fetch name from command or fallback
+const nameArg = capitalize(process.argv[2] || process.env.USER || process.env.USERNAME || "world");
+
+// output message
+console.log(`Hello ${nameArg}`);
+
+function capitalize(str) {
+	return str
+	.trim()
+	.toLowerCase()
+	.split(' ')
+	.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+	.join(' ');
+}
