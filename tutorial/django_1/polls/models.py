7297f03c5a92ec4a856827d6a354da43a4aff285
vincidax <vincentdx18@gmail.com>
2025-11-19 21:51:32 +0200

    hhhh

--- tutorial/django_1/polls/models.py
+++ tutorial/django_1/polls/models.py
@@ -1,31 +1,23 @@
+import datetime
+
 from django.db import models
-
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+from django.utils import timezone
 
 
-class Musician(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    instrument = models.CharField(max_length=100)
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
 
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-class Album(models.Model):
-    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    release_date = models.DateField()
-    num_stars = models.IntegerField()
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
-class Shirt(models.Model):
-    SHIRT_SIZES = {
-        "S": "Small",
-        "M": "Medium",
-        "L": "Large",
-    }
-    name = models.CharField(max_length=60)
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
-
-class Fruit(models.Model):
-    name = models.CharField(max_length=100, primary_key=True)
-
+    def __str__(self):
+        return self.choice_text
---
+++ tutorial/js_basics/test.js
@@ -0,0 +1,3 @@
+console.log("The process is:", process.pid);
+console.log("Version of Nodejs ", process.versions.node);
+console.log("Command line arguments", process.argv);
